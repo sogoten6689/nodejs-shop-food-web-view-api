2e4a182f931bd0aa4fcbdf4467f64ebf858ef788
Lam Nguyen <lamnn@imt-soft.com>
2020-11-03 17:58:17 +0700

    - update ui home page

---
+++ controllers/homeController.js
@@ -0,0 +1,19 @@
+var db =  require('../models/index.js');
+var User = db.user;
+var Food = db.foods;
+
+module.exports = {
+    index: async function(req, res) {
+        res.render('index',{
+            foods: Food.findAll(),
+            foodCount: 5,
+        });
+    },
+
+    search: async function(req, res) {
+        var result = await User.findAll({limit: 10});
+        res.render('users/index', {
+            users: result // render lại trang users/index với biến users bây giờ chỉ bao gồm các kết quả phù hợp
+        });
+    },
+};
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,10 @@
 var express = require('express');
 var router = express.Router();
+const homeController = require('../controllers/homeController');
 
 /* GET home page. */
+
+router.get('/', homeController.index);
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
